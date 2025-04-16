bc5079668b16bad77a2fa448576fe159a2b1147a
alkrivosheev <alkrivosheev@gmail.com>
2025-04-16 09:47:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sq-ch12-ex1
+# sq-ch12-ex1
---
+++ src/main/java/ru/job4j/sqch12ex1/SqCh12Ex1Application.java
@@ -0,0 +1,13 @@
+package ru.job4j.sqch12ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh12Ex1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqCh12Ex1Application.class, args);
+    }
+
+}
